117c0063796216f9888cdc708c0597231725cd89
MahyaR-Kd <mahyarkakvand96@gmail.com>
2024-07-29 21:27:35 +0330

    feat: add types module

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * from './deep-partial.type';
+export * from './entity-condition.type';
+export * from './find-options.type';
+export * from './paginated-data.type';
+export * from './pagination-options.type';
---
+++ src/types/paginated-data.type.ts
@@ -0,0 +1,4 @@
+export interface IPaginatedData<T> {
+  data: T[];
+  count: number;
+}
