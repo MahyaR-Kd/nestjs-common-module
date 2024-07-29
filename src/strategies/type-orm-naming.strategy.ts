5bee9c850c6d95cec974451ab3dc53f9d850ae9d
MahyaR-Kd <mahyarkakvand96@gmail.com>
2024-07-29 20:32:26 +0330

    feat: make strategies module

---
+++ src/strategies/type-orm-naming.strategy.ts
@@ -0,0 +1,17 @@
+import { DefaultNamingStrategy, NamingStrategyInterface } from 'typeorm';
+import { snakeCase } from 'typeorm/util/StringUtils';
+
+export class SnakeNamingStrategy
+  extends DefaultNamingStrategy
+  implements NamingStrategyInterface
+{
+  columnName(
+    propertyName: string,
+    customName: string,
+    embeddedPrefixes: string[],
+  ): string {
+    return snakeCase(
+      embeddedPrefixes.concat(customName || propertyName).join('_'),
+    );
+  }
+}
