20227c5a90f0a712595b38a9deded9488b72032b
MahyaR-Kd <mahyarkakvand96@gmail.com>
2024-07-29 21:41:29 +0330

    fix: refit core-crud.controller.ts imports

--- src/controllers/core-crud.controller.ts
+++ src/controllers/core-crud.controller.ts
@@ -5,13 +5,13 @@ import {
 } from '@nestjs/common';
 import { Paginate, Paginated } from 'nestjs-paginate';
 import { PaginateConfig } from 'nestjs-paginate/lib/paginate';
-import { PaginationQueryCustom } from '../interfaces/pagination-query';
-import { SharedMessages } from '../enums/shared-messages.enum';
+import { PaginationQueryCustomDto } from '../dto';
+import { SharedMessages } from '../enums';
 import { SuccessResponse } from '../dto';
 import { BaseResponseWithActionDates } from '../dto';
 import { IdDto } from '../dto';
 import { CoreCrudService } from '../services';
-import { ParentEntity } from '../entities/parent.entity';
+import { ParentEntity } from '../entities';
 
 export class CoreCrudController<
   T extends ParentEntity,
@@ -25,7 +25,7 @@ export class CoreCrudController<
   ) {}
 
   async findAll(
-    @Paginate() query: PaginationQueryCustom,
+    @Paginate() query: PaginationQueryCustomDto,
     paginateConfig: PaginateConfig<T>,
   ): Promise<Paginated<T>> {
     const response = await this.coreService.findAllWithPagination(
@@ -43,22 +43,22 @@ export class CoreCrudController<
     if (!response)
       throw new NotFoundException(SharedMessages.RESOURCE_NOT_FOUND);
 
-    return new SuccessResponse<ResponseDto>(
-      new this.responseDto(response),
-      SharedMessages.SUCCESSFUL,
-      HttpStatus.OK,
-    );
+    return new SuccessResponse<ResponseDto>({
+      data: new this.responseDto(response),
+      message: SharedMessages.SUCCESSFUL,
+      statusCode: HttpStatus.OK,
+    });
   }
 
   async create(createDto: CreateDto): Promise<SuccessResponse<ResponseDto>> {
     const response = await this.coreService.create(createDto);
     if (!response) throw new BadRequestException(SharedMessages.CREATE_FAILED);
 
-    return new SuccessResponse<ResponseDto>(
-      new this.responseDto(response),
-      SharedMessages.SUCCESSFUL,
-      HttpStatus.CREATED,
-    );
+    return new SuccessResponse<ResponseDto>({
+      data: new this.responseDto(response),
+      message: SharedMessages.SUCCESSFUL,
+      statusCode: HttpStatus.CREATED,
+    });
   }
 
   async update(
@@ -69,32 +69,32 @@ export class CoreCrudController<
     if (!response) throw new BadRequestException(SharedMessages.UPDATE_FAILED);
 
     const foundItem = await this.coreService.findOneById(id);
-    return new SuccessResponse<ResponseDto>(
-      new this.responseDto(foundItem),
-      SharedMessages.SUCCESSFUL,
-      HttpStatus.OK,
-    );
+    return new SuccessResponse<ResponseDto>({
+      data: new this.responseDto(foundItem),
+      message: SharedMessages.SUCCESSFUL,
+      statusCode: HttpStatus.OK,
+    });
   }
 
   async delete({ id }: IdDto): Promise<SuccessResponse<void>> {
     const result = await this.coreService.deleteById(id);
     if (!result) throw new BadRequestException(SharedMessages.DELETE_FAILED);
 
-    return new SuccessResponse<void>(
-      undefined,
-      SharedMessages.SUCCESSFUL,
-      HttpStatus.OK,
-    );
+    return new SuccessResponse<void>({
+      data: undefined,
+      message: SharedMessages.SUCCESSFUL,
+      statusCode: HttpStatus.OK,
+    });
   }
 
   async softDelete({ id }: IdDto): Promise<SuccessResponse<void>> {
     const result = await this.coreService.softDeleteById(id);
     if (!result) throw new BadRequestException(SharedMessages.DELETE_FAILED);
 
-    return new SuccessResponse<void>(
-      undefined,
-      SharedMessages.SUCCESSFUL,
-      HttpStatus.OK,
-    );
+    return new SuccessResponse<void>({
+      data: undefined,
+      message: SharedMessages.SUCCESSFUL,
+      statusCode: HttpStatus.OK,
+    });
   }
 }
