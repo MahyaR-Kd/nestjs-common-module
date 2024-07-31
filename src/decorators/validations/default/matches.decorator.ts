c3c0737ae998f63dc37986d6bfb31b86adc620a7
MahyaR-Kd <mahyarkakvand96@gmail.com>
2024-07-31 21:03:56 +0330

    fix: refit custom Matches validation decorator to handle multi type of pattern

--- src/decorators/validations/default/matches.decorator.ts
+++ src/decorators/validations/default/matches.decorator.ts
@@ -1,13 +1,15 @@
 import { Matches as matches } from 'class-validator';
 
 export function Matches(
-  pattern,
+  pattern: RegExp | string,
   options?: { each?: boolean },
 ): PropertyDecorator {
+  const machPattern =
+    typeof pattern === 'string' ? new RegExp(pattern) : pattern;
   return function (target: any, propertyKey: string | symbol): void {
-    matches(pattern, Object.assign({ message: 'validation.PATTERN' }, options))(
-      target,
-      propertyKey,
-    );
+    matches(
+      machPattern,
+      Object.assign({ message: 'validation.PATTERN' }, options),
+    )(target, propertyKey);
   };
 }
